05363a958633aa3c1d8e04c2b86540c06427fc52
8devabhi <abhinava.dev@gmail.com>
2023-12-12 20:41:00 +0530

    setups for jest and styled components

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  compiler: {
+    styledComponents: {
+      displayName: true,
+      ssr: true, // Make sure to use a colon here instead of a comma
+      fileName: true,
+      topLevelImportPaths: [],
+      meaninglessFileNames: ["index"],
+      cssProp: true,
+      namespace: "",
+      minify: true,
+      transpileTemplateLiterals: true,
+      pure: true,
+    },
+    removeConsole: true,
+  },
+};
